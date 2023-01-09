26854648d003551d33bfa0bd67ca88bc3e7e81cc
Howard Wang <idontwannarock@gmail.com>
2023-01-09 13:35:24 +0800

    feat: remove GreetService

--- chat-client/src/App.js
+++ chat-client/src/App.js
@@ -1,25 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { GreetServiceClient } from './proto/chat_grpc_web_pb';
-import { Greeting, GreetRequest } from './proto/chat_pb';
-
-const client = new GreetServiceClient(process.env.REACT_APP_API_URL);
-
 function App() {
-
-	const grpcCall = () => {
-		const greeting = new Greeting();
-		greeting.setFirstName('John');
-		greeting.setLastName('Doe');
-
-		const request = new GreetRequest();
-		request.setGreeting(greeting);
-
-		client.greet(request, {}, function (err, response) {
-			console.log(response.getResult());
-		});
-	}
 
   return (
     <div className="App">
@@ -29,7 +11,6 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <div>
-					<button onClick={grpcCall}>Click</button>
 				</div>
       </header>
     </div>
